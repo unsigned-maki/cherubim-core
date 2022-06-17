cec899ab288e60fa99a0e56ba90ec04b1d4ee769
Niclas Nell <nel20326@spengergasse.at>
2022-06-17 08:55:24 +0200

    feat(parser chain): add more meaningfull error

--- parser/chain.py
+++ parser/chain.py
@@ -27,7 +27,7 @@ class Chain:
                         )
                         i = braces[current.id]
                     else:
-                        raise ValueError("Error")
+                        raise ValueError("Invalid Syntax")
                         #  TODO: raise error
                 case "SYN_BRACE_CLOSE":
                     continue
@@ -35,36 +35,36 @@ class Chain:
                     if previous.type in cr["LTRL"]["previous"] and next.type in cr["LTRL"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
-                        raise ValueError("Error")
+                        raise ValueError("Invalid Syntax")
                         #  TODO: raise error
                 case "OP_IF" | "OP_IFF" | "OP_AND" | "OP_NAND" | "OP_OR" | "OP_XOR" | "OP_NOR" | "OP_XNOR":
                     if previous.type in cr["OP"]["previous"] and next.type in cr["OP"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
-                        raise ValueError("Error")
+                        raise ValueError("Invalid Syntax")
                         #  TODO: raise error
                 case "OP_NOT":
                     if previous.type in cr["OP_NOT"]["previous"] and next.type in cr["OP_NOT"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
-                        raise ValueError("Error")
+                        raise ValueError("Invalid Syntax")
                         #  TODO: raise error
                 case "OP_DEFINE":
                     if previous.type in cr["OP_DEFINE"]["previous"] and next.type in cr["OP_DEFINE"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
-                        raise ValueError("Error")
+                        raise ValueError("Invalid Syntax")
                         #  TODO: raise error
                 case "ID_IDENTIFIER":
                     if previous.type in cr["ID"]["previous"] and next.type in cr["ID"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
-                        raise ValueError("Error")
+                        raise ValueError("Invalid Syntax")
                         #  TODO: raise error
                 case "NIL":
                     continue
                 case _:
-                    raise ValueError("Error")
+                    raise ValueError("Invalid Syntax")
                     #  TODO: raise error
 
             i += 1
