caf8aa7a6d47ad2ffdc5d7a196aa088f1e0af788
Niclas Nell <nel20326@spengergasse.at>
2022-06-17 02:23:50 +0200

    feat(parser chain): add temporary errors

--- parser/chain.py
+++ parser/chain.py
@@ -1,6 +1,7 @@
-from block import Block
+from .block import Block
 from lexer.tokens import Token, TOKEN
-from rules import CHAIN_RULES as cr
+from .rules import CHAIN_RULES as cr
+
 
 class Chain:
 
@@ -20,32 +21,40 @@ class Chain:
                         self.inner.append(
                             Block(
                                 "BLOCK_CHAIN",
-                                Chain(tokens, current.id, braces[current.id], braces, memory)
+                                Chain(tokens, current.id + 1, braces[current.id] - 1, braces, memory)
                             )
                         )
                     else:
+                        raise ValueError("Error")
                         #  TODO: raise error
+                case "SYN_BRACE_CLOSE":
+                    continue
                 case "LTRL_TRUE" | "LTRL_FALSE":
                     if previous.type in cr["LTRL"]["previous"] and next.type in cr["LTRL"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
+                        raise ValueError("Error")
                         #  TODO: raise error
                 case "OP_IF" | "OP_IFF" | "OP_AND" | "OP_NAND" | "OP_OR" | "OP_XOR" | "OP_NOR" | "OP_XNOR":
                     if previous.type in cr["OP"]["previous"] and next.type in cr["OP"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
+                        raise ValueError("Error")
                         #  TODO: raise error
                 case "OP_NOT":
                     if previous.type in cr["OP_NOT"]["previous"] and next.type in cr["OP_NOT"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
+                        raise ValueError("Error")
                         #  TODO: raise error
                 case "ID_IDENTIFIER":
                     if previous.type in cr["ID"]["previous"] and next.type in cr["ID"]["next"]:
                         self.inner.append(Block(current.type, current.literal))
                     else:
+                        raise ValueError("Error")
                         #  TODO: raise error
                 case "NIL":
                     continue
                 case _:
+                    raise ValueError("Error")
                     #  TODO: raise error
